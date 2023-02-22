aecf4d4649b2b47357ce159206c74e6608418e47
Hussein <khadrahussein4@gmail.com>
2023-02-22 18:41:01 +0200

    created class structure

---
+++ addressBookv1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class Person
+{
+    public:
+        string firstName;
+        string lastName;
+};
+
+class Address
+{
+    public:
+    string streetName,cityName,district;
+    int postCode;
+};
+
+class emailAddress
+{
+    public:
+    string email;
+};
+
+class Telephone
+{
+    public:
+    string phoneNumber;
+};
+
+//Main class
+class AddressBook
+{
+    
+};

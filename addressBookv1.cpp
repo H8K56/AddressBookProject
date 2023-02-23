ca10c14e6c6cb3b768a8fecbc49412d6d67ffc38
Hussein <khadrahussein4@gmail.com>
2023-02-23 10:12:20 +0200

    Created formats for each element my addressBook

--- addressBookv1.cpp
+++ addressBookv1.cpp
@@ -25,18 +25,17 @@ class AddressBook
     class Address
     {
         private:
-        string streetName,cityName,district;
-        int postCode;
+        string streetName,cityName,district,postCode;
         
         public:
-        Address(string street,string city,string district,int post_code)
+        Address(string street,string city,string district,string post_code)
         {
             this->streetName = street;
             this->cityName = city;
             this->district = district;
             this->postCode = post_code;
         }
-        string getAddress(string,string,string,int);
+        string getAddress(string,string,string,string);
     };
     class emailAddress
     {
@@ -63,3 +62,22 @@ class AddressBook
         string getTelePhone(string);
     };
 };
+//Functions that set the format for the address book
+string AddressBook::Person::getName(string first,string last)
+{
+    string fullName = first + " " + last;
+    return fullName;
+}
+string AddressBook::Address::getAddress(string street,string city,string district,string postCode)
+{
+    string addressOfPerson = street + "," + city + "," + district + "," + postCode;
+    return addressOfPerson;
+}
+string AddressBook::emailAddress::getEmailAddress(string e_mail)
+{
+    return e_mail; 
+}
+string AddressBook::Telephone::getTelePhone(string phone)
+{
+    return phone;
+}
